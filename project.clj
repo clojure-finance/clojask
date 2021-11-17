c54e146da341fe4a118b52bc7d56db09888b22a8
Yuchen Liu <43634213+hkulyc@users.noreply.github.com>
2021-11-17 22:23:27 +0800

    ready for the first release

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject clojask "0.1.0-SNAPSHOT"
+(defproject clojask "1.0.0"
   :description "FIXME: write description"
   :url "http://example.com/FIXME"
   :license {:name "EPL-2.0 OR GPL-2.0-or-later WITH Classpath-exception-2.0"
