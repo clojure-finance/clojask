85251518b1aaf05aaaba47dcc87323413123db79
Sung ho <sungho.chu8567@gmail.com>
2022-03-02 12:47:37 +0800

    Small update to function description

--- project.clj
+++ project.clj
@@ -1,22 +1,22 @@
-(defproject clojask "1.2.0"
+(defproject com.github.clojure-finance/clojask "1.2.0"
   :description "Data analysis and manipulation library with parallel computing for larger-than-memory datasets"
   :url "https://github.com/clojure-finance/clojask"
   :license {:name "EPL-2.0 OR GPL-2.0-or-later WITH Classpath-exception-2.0"
             :url "https://www.eclipse.org/legal/epl-2.0/"}
   :dependencies [[org.clojure/clojure "1.10.1"]
-                ;;  [org.clojure/math.numeric-tower "0.0.4"]
+                 [org.clojure/math.numeric-tower "0.0.4"]
                  [org.clojure/data.csv "1.0.0"]
                  ^{:voom {:repo "git@github.com:onyx-platform/onyx.git" :branch "master"}}
                  [org.onyxplatform/onyx "0.14.6"]
-                ;;  [techascent/tech.ml.dataset "5.17" :exclusions [[ch.qos.logback/logback-classic][org.slf4j/slf4j-api]]]
+                 [techascent/tech.ml.dataset "5.17" :exclusions [[ch.qos.logback/logback-classic][org.slf4j/slf4j-api]]]
                  [com.google.code.externalsortinginjava/externalsortinginjava "0.6.0"]]
   :repl-options {:init-ns clojask.debug
                  :timeout 180000}
-  :plugins [[lein-update-dependency "0.1.2"]]
-  :main ^:skip-aot clojask.debug/-main
-  :source-paths      ["src/main/clojure"]
-  :java-source-paths ["src/main/java"]
-  :test-paths        ["test/clojask"]
+;;   :plugins [[lein-update-dependency "0.1.2"]]
+;;   :main ^:skip-aot clojask.debug/-main
+;;   :source-paths      ["src/main/clojure"]
+;;   :java-source-paths ["src/main/java"]
+;;   :test-paths        ["test/clojask"]
   ;:java-test-paths   ["test/java"]
   ;;:injections [(.. System (setProperty "clojure.core.async.pool-size" "8"))]
   )
