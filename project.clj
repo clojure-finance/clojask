de8ee137dcc9139aa18522d0e8075cca10e97d50
Yuchen Liu <43634213+hkulyc@users.noreply.github.com>
2022-06-09 02:23:53 +0800

    Update project.clj

--- project.clj
+++ project.clj
@@ -4,11 +4,11 @@
   :license {:name "EPL-2.0 OR GPL-2.0-or-later WITH Classpath-exception-2.0"
             :url "https://www.eclipse.org/legal/epl-2.0/"}
   :dependencies [[org.clojure/clojure "1.10.1"]
-                 [org.clojure/math.numeric-tower "0.0.4"]
+                ;;  [org.clojure/math.numeric-tower "0.0.4"]
                  [org.clojure/data.csv "1.0.0"]
                  ^{:voom {:repo "git@github.com:onyx-platform/onyx.git" :branch "master"}}
                  [org.onyxplatform/onyx "0.14.6"]
-                 [techascent/tech.ml.dataset "5.17" :exclusions [[ch.qos.logback/logback-classic][org.slf4j/slf4j-api]]]
+                ;;  [techascent/tech.ml.dataset "5.17" :exclusions [[ch.qos.logback/logback-classic][org.slf4j/slf4j-api]]]
                  [com.google.code.externalsortinginjava/externalsortinginjava "0.6.0"]]
   :repl-options {:init-ns clojask.debug
                  :timeout 180000}
