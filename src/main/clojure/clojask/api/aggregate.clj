afebb05888241e1fc840fc73e1602cb4864a846e
Yuchen Liu <43634213+hkulyc@users.noreply.github.com>
2022-08-22 23:10:30 +0800

    update api doc

--- src/main/clojure/clojask/api/aggregate.clj
+++ src/main/clojure/clojask/api/aggregate.clj
@@ -1,18 +1,24 @@
 (ns clojask.api.aggregate
-  (:refer-clojure :exclude [max min]))
+  (:refer-clojure :exclude [max min count]))
 "Contains implemented simple aggregation functions"
 
 (def start)
 
 (defn max
   [a b]
-  (if (and (not= a start) (> (compare a b) 0))
+  (if (and (> (compare a b) 0) (not= a start))
     a
     b))
 
 (defn min
   [a b]
-  (if (and (not= a start) (< (compare a b) 0))
+  (if (and (< (compare a b) 0) (not= a start))
     a
     b))
+
+(defn count
+  [a b]
+  (if (= a start)
+    1
+    (inc a)))
 
