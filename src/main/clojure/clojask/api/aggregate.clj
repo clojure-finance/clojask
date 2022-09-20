aac32bb06eb5d0e58203dc4833b521058490939c
Yuchen Liu <43634213+hkulyc@users.noreply.github.com>
2022-09-21 02:09:18 +0800

    more agg functions

--- src/main/clojure/clojask/api/aggregate.clj
+++ src/main/clojure/clojask/api/aggregate.clj
@@ -1,5 +1,5 @@
 (ns clojask.api.aggregate
-  (:refer-clojure :exclude [max min count]))
+  (:refer-clojure :exclude [max min sum count]))
 "Contains implemented simple aggregation functions"
 
 (def start)
@@ -7,6 +7,8 @@
 ;; (defn aggre-func
 ;;   "prev value could be start"
 ;;   [prev new])
+
+;; single row aggregation functions
 
 (defn max
   [a b]
@@ -20,9 +22,29 @@
     b
     a))
 
+(defn sum
+  [a b]
+  (if (= a start)
+    b
+    (+ a b)))
+
 (defn count
   [a b]
   (if (= a start)
     1
     (inc a)))
+
+;; multi-row aggregation functions
+
+(defn top3
+  [a b]
+  (cond
+    (= start a) [b]
+    :else (take 3 (sort (conj a b)))))
+
+(defn bottom3
+  [a b]
+  (cond
+    (= start a) [b]
+    :else (take 3 (sort (fn [a b] (compare b a)) (conj a b)))))
 
--- src/main/clojure/clojask/api/gb_aggregate.clj
+++ src/main/clojure/clojask/api/gb_aggregate.clj
@@ -1,18 +1,37 @@
 (ns clojask.api.gb-aggregate
-  (:require [clojask.api.aggregate :as aggre])
-  (:refer-clojure :exclude [max min]))
+  (:require [clojask.api.aggregate :as agg])
+  (:refer-clojure :exclude [max min sum count]))
 "Contains the implemented function for group-by aggregation functions"
 
-;; (defn- compare
-;;   "Specially considering nil"
-;;   [a b]
-;;   ())
+;; (defn aggre-func
+;;   "function that can be applied on a collection"
+;;   [list])
+
+;; single row aggregation functions
 
 (defn max
   [list]
-  (reduce aggre/max list))
+  (reduce agg/max list))
 
 (defn min
   [list]
-  (reduce aggre/min list))
+  (reduce agg/min list))
+
+(defn sum
+  [list]
+  (reduce + list))
+
+(defn count
+  [list]
+  (count list))
+
+;; multi-row aggregation functions
+
+(defn top3
+  [list]
+  (reduce agg/top3 agg/start list))
+
+(defn bottom3
+  [list]
+  (reduce agg/bottom3 agg/start list))
 
