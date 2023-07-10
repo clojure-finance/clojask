42e5b7c87368655ba90a41c46433881f23ce3f08
skylee03 <1178715749@qq.com>
2023-07-10 13:03:21 +0800

    feat: aggregation functions mean, mode, median, sd, skew

--- src/main/clojure/clojask/api/gb_aggregate.clj
+++ src/main/clojure/clojask/api/gb_aggregate.clj
@@ -25,6 +25,56 @@
   [list]
   (clojure.core/count list))
 
+(defn mean
+  [list]
+  (let [sum (apply + list)
+        count (count list)]
+    (if (pos? count)
+      (/ sum count)
+      0)))
+
+(defn mode
+  [list]
+  (let [freqs (frequencies list)
+        occurrences (clojure.core/group-by val freqs)
+        modes (last (sort occurrences))
+        modes (->> modes
+                   val
+                   (map key))]
+    modes))
+
+(defn median
+  [list]
+  (let [sorted (sort list)
+        cnt (count sorted)
+        halfway (quot cnt 2)]
+    (if (odd? cnt)
+      (nth sorted halfway)
+      (let [bottom (dec halfway)
+            bottom-val (nth sorted bottom)
+            top-val (nth sorted halfway)]
+        (mean [bottom-val top-val])))))
+
+(defn sd
+  [list]
+  (let [avg (mean list)
+        squares (for [x list]
+                  (let [x-avg (- x avg)]
+                    (* x-avg x-avg)))
+        total (count list)]
+    (if (= 1 total)
+      0
+      (-> (/ (apply + squares)
+             (- total 1))
+          (Math/sqrt)))))
+
+(defn skew
+  [list]
+  (let [mean (mean list)
+        median (median list)
+        sd (sd list)]
+    (* 3 (/ (- mean median) sd))))
+
 ;; multi-row aggregation functions
 
 (defn smallest3
